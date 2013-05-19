45a526547ba220d25c8dade086e13ff622f2b1be
Steve Lounsbury <steve.lounsbury@gmail.com>
2013-05-19 15:34:47 -0400

    Add not_complete() and toggle() methods along with tests. Add tests for due date and completion dates as separate attributes (currently 3 tests not passing). Start implementation of separate due and completion dates.

--- spec/task.spec.js
+++ spec/task.spec.js
@@ -45,29 +45,32 @@ describe("task", function() {
 	});
 	it("should recognize dates in YYYY-MM-DD format", function() {
 		var task = new Task("(A) 2013-05-24 Do something interesting +project @context");
-		expect(task.date).toEqual(new Date("May 24, 2013"));
+		expect(task.due_date).toEqual(new Date("May 24, 2013"));
 		task = new Task("2013-05-24 Do something interesting +project @context");
-		expect(task.date).toEqual(new Date("May 24, 2013"));
+		expect(task.due_date).toEqual(new Date("May 24, 2013"));
 	});
 	it("should have a null date if no date present", function() {
 		var task = new Task("Hello world! +project1 +project2");
-		expect(task.date).toBe(null);
+		expect(task.due_date).toBe(null);
+		expect(task.completed_date).toBe(null);
 	});
 	it("should not recognize malformed dates", function() {
 		var task = new Task("(A) 20-05-2024 Do something interesting +project @context");
-		expect(task.date).toBe(null);
+		expect(task.due_date).toBe(null);
+		expect(task.completed_date).toBe(null);
 	});
 	it("should recognize overdue tasks", function() {
 		var task = new Task("(A) 2000-05-24 Do something in the past.");
-		expect(task.isOverdue()).toBe(true);
+		expect(task.is_overdue()).toBe(true);
 		task = new Task("(A) 3000-05-24 Do something in the future.");
-		expect(task.isOverdue()).toBe(false);
+		expect(task.is_overdue()).toBe(false);
 		task = new Task("(A) Do something in at no particular time.");
-		expect(task.isOverdue()).toBe(false);
+		expect(task.is_overdue()).toBe(false);
 	});
 	it("should recognize completed tasks", function() {
 		var task = new Task("x 2013-05-24 This is done.");
 		expect(task.done).toBe(true);
+		expect(task.completed_date).toEqual(new Date("May 24, 2013"));
 	});
 	it("should not consider incomplete tasks as done", function() {
 		var task = new Task("2013-05-24 This is not done.");
@@ -81,5 +84,28 @@ describe("task", function() {
 	it("should provide the text of the task", function() {
 		var task = new Task("(A) 2013-05-24 Do something interesting +project @context");
 		expect(task.text).toBe("Do something interesting");
+	});
+	it("should be able to be marked as incomplete", function() {
+		var task = new Task("x This is a completed task");
+		task.not_complete();
+		expect(task.done).toBe(false);
+	});
+	it("should be able to be toggled", function() {
+		var task = new Task("This is an incomplete task");
+		task.toggle();
+		expect(task.done).toBe(true);
+		task.toggle();
+		expect(task.done).toBe(false);
+	});
+	it("should recognize completion dates", function() {
+		var task = new Task("x 2013-05-24 This is done.");
+		expect(task.due_date).toBe(null);
+		expect(task.completed_date).toEqual(new Date("May 24, 2013"));
+	});
+	it("should set the completion date when completed", function() {
+		var task = new Task("2013-05-24 This is not done.");
+		var today = new Date();
+		task.complete();
+		expect(task.completed_date.toDateString()).toBe(today.toDateString());
 	});
 });
