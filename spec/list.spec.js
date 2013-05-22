523dd97d1c5cc6ed38870e94655b3242de91e952
Steve Lounsbury <steve.lounsbury@gmail.com>
2013-05-22 00:00:22 -0400

    Add initial implementation of todo list, does almost nothing at this point.

---
+++ spec/list.spec.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var fs = require('fs');
+var List = require('../lib/todo_list');
+var Task = require('../lib/todo_task');
+
+describe("list", function() {
+	var list;
+	var data = fs.readFileSync(__dirname+'/data/todo.txt').toString();
+	beforeEach(function() {
+		list = new List(data);
+	});
+	it("should create a list of tasks", function() {
+		for (var i = 0; i < list.tasks.length; i++) {
+			expect(list.tasks[i] instanceof Task).toBeTruthy();
+		}
+		expect(list.tasks.length > 0).toBeTruthy();
+	});
+});
