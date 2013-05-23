a2a00c8036bf99e4e01919b6a429956b946ecaed
Steve Lounsbury <steve.lounsbury@gmail.com>
2013-05-22 23:31:28 -0400

    Add ability to filter lists by project, priority and context. Filters can be chained.

--- spec/list.spec.js
+++ spec/list.spec.js
@@ -17,4 +17,35 @@ describe("list", function() {
 		}
 		expect(list.tasks.length > 0).toBeTruthy();
 	});
+	it('should filter by priority', function() {
+		list.by_priority('A').tasks.forEach(function(task) {
+			expect(task.priority).toBe('A');
+		});
+		expect(list.by_priority('A').tasks.length > 0).toBeTruthy();
+	});
+	it('should filter by context', function() {
+		list.by_context('@context').tasks.forEach(function(task) {
+			expect(task.contexts).toContain('@context');
+		});
+		expect(list.by_context('@context').tasks.length > 0).toBeTruthy();
+	});
+	it('should filter by project', function() {
+		list.by_project('+project').tasks.forEach(function(task) {
+			expect(task.projects).toContain('+project');
+		});
+		expect(list.by_project('+project').tasks.length > 0).toBeTruthy();
+	});
+	it('should filter by project, context and priority', function() {
+		var filtered = list.by_project('+project').
+							by_context('@context').
+							by_priority('C');
+		expect(filtered instanceof List).toBeTruthy();
+		filtered.tasks.forEach(function(task) {
+			expect(task.priority).toBe('C');
+			expect(task.projects).toContain('+project');
+			expect(task.contexts).toContain('@context');
+		});
+		expect(filtered.tasks.length > 0).toBeTruthy();
+	});
+
 });
